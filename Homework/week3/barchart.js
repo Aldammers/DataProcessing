c5b026933b3672533e08ce42f80bd19a979b9981
Aldammers <31691842+Aldammers@users.noreply.github.com>
2018-02-23 19:16:57 +0100

    Update barchart.js

--- Homework/week3/barchart.js
+++ Homework/week3/barchart.js
@@ -1,3 +1,11 @@
+/*
+Week 3 
+Alexander Dammers
+10528415
+Show bar charts with input of JSON data
+*/
+
+
 // set area for barcharts
 var margin = {top: 50, right: 35, bottom: 100, left: 50},
     width = 800 - margin.left - margin.right,
@@ -11,7 +19,7 @@ var chart = d3.select(".chart")
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    
+    console.log(chart)
     
 // scale x axis
 var x = d3.scale.ordinal()
@@ -35,7 +43,8 @@ var yAxis = d3.svg.axis()
     .ticks(10, "%");
     
     
-d3.json(Faillissementen.json, function(error, data) {console.log(data);
+d3.json(Faillissementen.json, function(error, data) {
+    console.log(data);
 
 
 
@@ -44,8 +53,8 @@ d3.json(Faillissementen.json, function(error, data) {console.log(data);
 
 
 // range scaling of data
-x.domain(data.map(function(d) { return d.Periode; }));
-y.domain([0, d3.max(data, function(d) { return d.Aantal; })]);
+    x.domain(data.map(function(d) { return d.Periode; }));
+    y.domain([0, d3.max(data, function(d) { return d.Aantal; })]);
 
 
 // d3.csv("Faillissementen_jan2018.csv", function(error, data) {
@@ -53,50 +62,48 @@ y.domain([0, d3.max(data, function(d) { return d.Aantal; })]);
 
 
 // create x axis and label it
-chart.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis)
-    .selectAll("text")
-    .attr("transform", "rotate(-90)")
-    .attr("x", "-0.4em")
-    .attr("dy", ".71em")
-    .style("text-anchor", "end")
-  //  .text("Aantal Faillissementen");
+    chart.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .selectAll("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", "-0.4em")
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Aantal Faillissementen");
 
 
-// create y axis and label it
-chart.append("g")
-    .attr("class", "y axis")
-   // .attr("transform", "translate(0," + height + ")")
-    .call(yAxis)
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end")
-    .text("Aantal Faillissementen");
+    // create y axis and label it
+    chart.append("g")
+        .attr("class", "y axis")
+    // .attr("transform", "translate(0," + height + ")")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Aantal Faillissementen");
   
  
-chart.append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 5)
-    .attr("dy", ".71em");
+    chart.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 5)
+        .attr("dy", ".71em");
     
     
-// draw the chartbars 
-chart.selectAll(".bar")
-    .data(data)
-   .enter().append("rect")
-    .attr("class", "bar")
-    .attr("x", function(d) { return x(d.Periode); })
-    .attr("y", function(d) { return y(d.Aantal); })
-    .attr("height", function(d) { return height - y(d.Aantal); })
-    .attr("width", x.rangeBand());
+    // draw the chartbars 
+    chart.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.Periode); })
+        .attr("y", function(d) { return y(d.Aantal); })
+        .attr("height", function(d) { return height - y(d.Aantal); })
+        .attr("width", x.rangeBand());
     
-})
-    
-    
+}) 
